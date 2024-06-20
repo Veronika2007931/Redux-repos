744d9b02c73fa8346e49cbb66dc182cec7ecb061
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2024-06-20 17:24:14 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,7 +16,12 @@ export function App(){
         <Route pats="/" element={<Layout/>}>
         <Route index element={<GreatsPage/>}/>
         <Route path="/cats" element={<CatsPage/>}/>
-        <Route pats="/cats/:catId" element={<CatDetailPage/>}/>
+        <Route pats="/cats/:catId" element={<CatDetailPage/>}>
+           <Route path='/discription' element={<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, quam laboriosam. Dicta debitis repellat quas inventore est, 
+            cum delectus eligendi laborum laudantium 
+            ea officiis ipsum non id impedit consequuntur praesentium!</p>} elment={<p>Опис котика</p>}/>
+            <Route path="/breeds_history" element={<p>Історія породи</p>}/> 
+          </Route>
         <Route path='/grossery' element={<h2>Grossery</h2>}/>
         <Route path="/colorPicker"  element={<ColorPickerPage/>}/>
         <Route path="/form"  element={<FormPage/>}/>
