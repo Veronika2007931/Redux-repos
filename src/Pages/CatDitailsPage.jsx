6c9840845cd8b3503da0465055b7e878175da4f9
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2024-06-24 00:56:38 +0300

    фвв

--- src/Pages/CatDitailsPage.jsx
+++ src/Pages/CatDitailsPage.jsx
@@ -1,9 +1,15 @@
-import { useParams } from "react-router-dom"
+import { useParams, Link } from "react-router-dom"
 
 const CatDetailPage=()=>{
     const {catId} = useParams()
     return(
+       <>
         <h1>Cat {catId}</h1>
+        <ul>
+            <li><Linl to="description">Description</Linl></li>
+            <li><Linl to="breeeds_history">History</Linl></li>
+        </ul>
+       </>
     )
 }
 
